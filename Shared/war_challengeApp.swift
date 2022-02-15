a6cfa7ba6e437fb3f480ad79d7209ab82fda0f86
akatzfey <katzfeyalex@gmail.com>
2022-02-15 15:50:19 -0800

    Initial Commit

---
+++ Shared/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  Shared
+//
+//  Created by Alexander Katzfey on 1/9/22.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
